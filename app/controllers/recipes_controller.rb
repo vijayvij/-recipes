e8f0f7968ce13e1d68bef23926131f89ba3b073e
vijayvij <vhat007@gmail.com>
2018-08-29 01:49:21 +0000

    completed CRUD OPTIONS

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,5 +7,44 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
 
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted successfully"
+    redirect_to recipes_path
+  end
+    
+  private
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
 end
