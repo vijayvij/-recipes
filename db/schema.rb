2d25d225a06e17315e789db9050631f59b2d6684
vijayvij <vhat007@gmail.com>
2018-08-29 23:49:27 +0000

    Completed admin functions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_125528) do
+ActiveRecord::Schema.define(version: 2018_08_29_231606) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_125528) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   create_table "recipes", force: :cascade do |t|
